e58e8d8d4128e5e96cc0cd6cacdc2ea6870aa5aa
Robert Sefer <rsefer@gmail.com>
2025-02-12 19:17:53 -0600

    Updates

--- setup.sh
+++ setup.sh
@@ -24,5 +24,3 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo groupadd docker
 sudo usermod -a -G docker $USER
-
-docker network create "tailscale_network"
